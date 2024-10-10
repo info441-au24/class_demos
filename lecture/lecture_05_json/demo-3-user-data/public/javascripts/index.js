6eceaf57be926540113b9d37a39770726d6261ba
Kyle <kmthayer@uw.edu>
2024-10-10 12:21:18 -0700

    save user data (no load yet)

--- lecture/lecture_05_json/demo-3-user-data/public/javascripts/index.js
+++ lecture/lecture_05_json/demo-3-user-data/public/javascripts/index.js
@@ -1,16 +1,39 @@
-async function getPterosaurs(){
-    const response = await fetch("api/getPterosaurs")
-    const pterosaurJson = await response.json()
+async function createUser(){
+    // get data from the html
+    const first_name = document.getElementById("first_name_input").value
+    const last_name = document.getElementById("last_name_input").value
+    const favorite_ice_cream = document.getElementById("favorite_ice_cream_input").value
 
-    // use map to turn each json datapoint into html
-    let pterosaurHtml = pterosaurJson.map(onePterosaur => {
-        return `
-        <div>
-            <p>${onePterosaur.Genus}</p>
-            <img src="${onePterosaur.img}" />
-        </div>
-        `
-    }).join("")
+    let myData = {
+        first_name: first_name,
+        last_name: last_name,
+        favorite_ice_cream: favorite_ice_cream
+    }
+    console.log(myData)
 
-    document.getElementById("results").innerHTML = pterosaurHtml
+    // send post request to server
+    const response = await fetch("api/users", {
+        method: "POST",
+        body: JSON.stringify(myData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+async function getUsers(){
+    const response = await fetch("api/users")
+//     const pterosaurJson = await response.json()
+
+//     // use map to turn each json datapoint into html
+//     let pterosaurHtml = pterosaurJson.map(onePterosaur => {
+//         return `
+//         <div>
+//             <p>${onePterosaur.Genus}</p>
+//             <img src="${onePterosaur.img}" />
+//         </div>
+//         `
+//     }).join("")
+
+//     document.getElementById("results").innerHTML = pterosaurHtml
 }
--- lecture/lecture_05_json/demo-3-user-data/routes/api/api.js
+++ lecture/lecture_05_json/demo-3-user-data/routes/api/api.js
@@ -2,22 +2,21 @@ import {promises as fs} from 'fs'
 import express from 'express'
 var router = express.Router()
 
+
+router.post('/users', async (req, res) => {
+    console.log(req.body)
+
+    await fs.writeFile("data/userData.json",
+        JSON.stringify(req.body)
+    )
+})
+
 router.get('/getPterosaurs', async (req, res) => {
-    // load data file
-    const pterosaurFile = await fs.readFile("data/pterosaur.json")
+    // // load data file
+    // const pterosaurFile = await fs.readFile("data/pterosaur.json")
 
-    // filter out data that doesn't have images
-    const pterosaurJson = JSON.parse(pterosaurFile)
-    const filteredPterosaurJson = pterosaurJson.filter(onePterosaur => {
-        if(onePterosaur.img != ""){
-            return true
-        } else{
-            return false
-        }
-    })
-
-    // return data file
-    res.json(filteredPterosaurJson)
+    // // return data file
+    // res.json(filteredPterosaurJson)
 })
 
 export default router
