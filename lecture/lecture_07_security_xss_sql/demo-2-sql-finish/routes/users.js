872100d7bb1875c2348f36fbd2dad4a3d4ab14c4
Kyle <kmthayer@uw.edu>
2024-10-17 11:32:39 -0700

    set up in memory sql database

--- lecture/lecture_07_security_xss_sql/demo-2-sql-finish/routes/users.js
+++ lecture/lecture_07_security_xss_sql/demo-2-sql-finish/routes/users.js
@@ -1,7 +1,32 @@
+import sqlite3 from 'sqlite3'
 import express from 'express';
 var router = express.Router();
 
-/* GET users listing. */
+// create/load database
+let db = new sqlite3.Database(':memory:', (err) => {
+  if(err){
+    return console.error(err.message)
+  }
+  console.log("Connected to the in-memory sqlite database")
+})
+
+db.serialize(() => {
+  db
+    .run('CREATE TABLE people(first_name text, last_name text)')
+    .run(`INSERT INTO people(first_name, last_name)
+          VALUES ("Kyle", "Thayer"),
+                 ("Kyle", "Chandler"),
+                 ("Jasmine", "Zhang"),
+                 ("David", "Pham")
+      `)
+    .run('CREATE TABLE secret_table(message text)')
+    .run(`INSERT INTO secret_table(message)
+        VALUES ('The password for Kyle is: pa55w0rd'),
+               ('The treasure is hidden on the 5th floor')
+      `)
+})
+
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
