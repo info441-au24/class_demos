234a3678409a28429926be943bb5d4a821385711
Kyle <kmthayer@uw.edu>
2024-10-24 12:06:35 -0700

    finish auth demo

--- lecture/lecture_09_sessions_auth/demo-2-auth/app.js
+++ lecture/lecture_09_sessions_auth/demo-2-auth/app.js
@@ -4,6 +4,33 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import sessions from 'express-session'
 
+// To install msal-node-wrapper, run:
+//     npm install https://gitpkg.now.sh/kylethayer/ms-identity-javascript-nodejs-tutorial-msal-node-v2-/Common/msal-node-wrapper?main
+
+import WebAppAuthProvider from 'msal-node-wrapper'
+// original msal-node-wrapper code is here 
+//    (https://github.com/Azure-Samples/ms-identity-javascript-nodejs-tutorial/tree/main/Common/msal-node-wrapper),
+//  but at the time of making this, the install command doesn’t work (npm install azure-samples/ms-identity-javascript-nodejs-tutorial) 
+
+const authConfig = {
+    auth: {
+        clientId: "Client ID or Application ID HERE",
+        authority: "https://login.microsoftonline.com/Paste_the_Tenant_directory_ID_Here",
+        clientSecret: "Client or Application secret here (NOT THE 'secret id', but the 'secret value')",
+        redirectUri: "/redirect"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: 3,
+        }
+    }
+};
+
+
 import usersRouter from './routes/users.js';
 
 import { fileURLToPath } from 'url';
@@ -13,6 +40,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var app = express();
+
+app.enable('trust proxy')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,6 +57,26 @@ app.use(sessions({
     resave: false
 }))
 
+const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
+app.use(authProvider.authenticate());
+
+
 app.use('/users', usersRouter);
+
+
+app.get('/signin', (req, res, next) => {
+    return req.authContext.login({
+        postLoginRedirectUri: "/", // redirect here after login
+    })(req, res, next);
+
+});
+app.get('/signout', (req, res, next) => {
+    return req.authContext.logout({
+        postLogoutRedirectUri: "/", // redirect here after logout
+    })(req, res, next);
+
+});
+app.use(authProvider.interactionErrorHandler());
+
 
 export default app;
--- lecture/lecture_09_sessions_auth/demo-2-auth/routes/users.js
+++ lecture/lecture_09_sessions_auth/demo-2-auth/routes/users.js
@@ -3,30 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if(req.session.userid){
-    res.send(`Here is where I would put information about you (${req.session.userid})`)
+  if(req.session.isAuthenticated){
+    res.send(`Here is where I would put information about you
+      with your name being: (${req.session.account.name})
+      and your username being: ${req.session.account.username}`)
   }else{
     res.status(403).send("Error: You must be logged in")
   }
 });
 
-router.post('/login', (req, res) => {
-
-  // check username and password
-  if(req.body.username == "kylethayer" && req.body.password == "asdasd"){
-    req.session.userid = "kylethayer"
-    res.send("you logged in!")
-  } else if(req.body.username == "anotheruser" && req.body.password == "pwd"){
-    req.session.userid = "anotheruser"
-    res.send("you logged in!")
-  } else{
-    res.status(403).send("wrong login info")
-  }
-})
-
-router.post('/logout', (req, res) =>{
-  req.session.destroy()
-  res.send("you are logged out")
-})
 
 export default router;
