361e8a24695b98713135dceafdb6c813ed249111
Kyle <kmthayer@uw.edu>
2024-11-12 11:51:50 -0800

    add stripe payment to server

--- lecture/lecture_13_payments/demo-1-store-end/routes/items.js
+++ lecture/lecture_13_payments/demo-1-store-end/routes/items.js
@@ -19,22 +19,14 @@ router.post("/saveCart", async (req, res) => {
     res.json({status: "success"})
 })
 
-router.get("/getCart", async (req, res) => {
-    if(!req.session || ! req.session.cartInfo){
-        // if there is no session or saved cart, just return empty cart
-        res.json([])
-        return
-    }
-
-    let cartInfo = JSON.parse(req.session.cartInfo)
-
+async function addPricesToCart(cartInfo, models){
     // add prices and names to cart
     
     //cartInfo should start like this: [{itemId: 32, itemCount: 2}, {itemId, 4335, item_count, 1}, ...]
 
     // look up in the db all items listed in my cart
     let cartItemIds = cartInfo.map(cartItem => cartItem.itemId)
-    let itemsInfo = await req.models.Item.find().where("_id").in(cartItemIds).exec()
+    let itemsInfo = await models.Item.find().where("_id").in(cartItemIds).exec()
 
     // itemsInfo will be an array of JSON, like this:
     // [{_id: 32, name: "orange", price: ...}, {_id: 4335, name: "apple", ...}]
@@ -64,7 +56,53 @@ router.get("/getCart", async (req, res) => {
         }
     })
 
+    return combinedCartInfo
+}
+
+router.get("/getCart", async (req, res) => {
+    if(!req.session || ! req.session.cartInfo){
+        // if there is no session or saved cart, just return empty cart
+        res.json([])
+        return
+    }
+
+    let cartInfo = JSON.parse(req.session.cartInfo)
+
+    // add item names and prices to the cart info
+    let combinedCartInfo = await addPricesToCart(cartInfo, req.models)
+
     res.json(combinedCartInfo)
+})
+
+async function calculateOrderAmount(req){
+    let cartInfo = JSON.parse(req.session.cartInfo)
+
+    // add item names and prices to the cart info
+    let combinedCartInfo = await addPricesToCart(cartInfo, req.models)
+
+    let totalCost = combinedCartInfo
+        .map(item => item.price * item.itemCount) // get cost for each item
+        .reduce((prev, curr) => prev + curr)
+    
+    return totalCost
+}
+
+//stripe endpoint
+router.post("/create-payment-intent", async (req, res) => {
+    // TODO: look up the order amount
+    let orderAmount = await calculateOrderAmount(req) * 100
+    console.log("order amount", orderAmount)
+
+    // create a PaymentIntent object with the order amount
+    const paymentIntent = await req.stripe.paymentIntents.create({
+        amount: orderAmount,
+        currency: "usd", // 'usd' is actually US cents for some reason (US dollars * 100)
+        automatic_payment_methods: {enabled: true}
+    })
+
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    })
 })
 
 export default router;
