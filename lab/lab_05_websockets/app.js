5c11d44fef4633e6791d168dbedad66cc295a8e1
wkxdavid <phamdavid722@gmail.com>
2024-11-13 01:53:24 -0800

    cleanup

--- lab/lab_05_websockets/app.js
+++ lab/lab_05_websockets/app.js
@@ -1,38 +1,35 @@
-import express from 'express'
-import enableWs from 'express-ws'
+import express from 'express';
+import enableWs from 'express-ws';
+const app = express();
+enableWs(app);
 
-const app = express()
-enableWs(app)
-
-// track websocket connections
-let socketCounter = 1
-let allSockets = []
+let socketCounter = 1;
+let allSockets = [];
 
 app.ws('/chatSocket', (ws, res) => {
-    // "ws" now has the connection info
-    let mySocketNum = socketCounter
-    socketCounter++
-    console.log(`user ${mySocketNum} connected via websocket`)
-    allSockets.push(ws)
+  let mySocketNum = socketCounter;
+  socketCounter++;
 
-    ws.on('message', chat => {
-        console.log(`msg (user ${mySocketNum}): ${chat}`)
+  console.log(`user ${mySocketNum} connected via websocket`);
+  allSockets.push(ws);
 
-        allSockets.forEach(socket => {
-            socket.send(mySocketNum + ": " + chat)
-        })
-    })
+  ws.on('message', (chat) => {
+    console.log(`msg (user ${mySocketNum}): ${chat}`);
+    allSockets.forEach((socket) => {
+      socket.send(mySocketNum + ': ' + chat);
+    });
+  });
 
-    ws.on('close', () => {
-        console.log(`user ${mySocketNum} disconnected`)
-        console.log("I should probably delete them from the array or something")
-    })
-})
+  ws.on('close', () => {
+    console.log(`user ${mySocketNum} disconnected`);
+    console.log('I should probably delete them from the array or something');
+  });
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + "/index.html")
-})
+  res.sendFile(process.cwd() + '/index.html');
+});
 
 app.listen(3000, () => {
-    console.log("Example app listening at http://localhost:3000")
-})
+  console.log('Example app listening at http://localhost:3000');
+});
