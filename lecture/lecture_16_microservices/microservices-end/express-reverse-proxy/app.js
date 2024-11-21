45a30223a0cc36c5d584520c55e962ff71ba7a2d
Kyle <kmthayer@uw.edu>
2024-11-21 11:38:31 -0800

    get reverse proxy working (and fixed) for react front end

--- lecture/lecture_16_microservices/microservices-end/express-reverse-proxy/app.js
+++ lecture/lecture_16_microservices/microservices-end/express-reverse-proxy/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import httpProxyMiddleware from 'http-proxy-middleware'
+const createProxyMiddleware = httpProxyMiddleware.createProxyMiddleware
 
 import usersRouter from './routes/users.js';
 
@@ -17,7 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// NOTE: No longer using out own copy of the front-end
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
@@ -32,5 +35,12 @@ app.get('/api/square', (req, res) => {
     let squared = num * num
     res.send("" + squared)
 })
+
+// TODO: Try getting static files from the front-end react server
+app.use('/*', createProxyMiddleware({
+        target: 'http://localhost:4000',
+        pathRewrite: (path, req) => req.baseUrl
+    })
+)
 
 export default app;
